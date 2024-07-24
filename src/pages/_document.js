ab0c07ca580f83077b2e60decf010c7c421a5bd9
Diyac004 <diyachaudhari004@gmail.com>
2024-07-24 00:36:51 -0700

    change

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -42,11 +42,11 @@ export default class MyDocument extends Document {
         stragety="afterInteractive"
         dangerouslySetInnerHTML={{
           __html:`
-          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','G-PYQSW4E9S7');`
+          window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-PYQSW4E9S7');`
 
         }}
         >              
