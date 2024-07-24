4832f97113b25b46fabf6fd668d3ccce5e0fd1fb
Diyac004 <diyachaudhari004@gmail.com>
2024-07-24 01:05:00 -0700

    change

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -32,25 +32,23 @@ export default class MyDocument extends Document {
     return (
       <Html lang='en-GB'>
         <Head>
-
-          <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap" rel="stylesheet"/>
-        
-          
         <script
         id ="gtm"
         stragety="afterInteractive"
         dangerouslySetInnerHTML={{
           __html:`
           (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-T7XVJ9HW');`
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-T7XVJ9HW');`
 
         }}
-        >              
-                     
-                  </script>
+        ></script>
+          <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap" rel="stylesheet"/>
+        
+          
+        
         </Head>
         <body>
           <Main />
