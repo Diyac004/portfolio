f28b54cb681ebc130f303cfe60bbe35a713a5a0e
Diyac004 <diyachaudhari004@gmail.com>
2024-07-24 00:26:05 -0700

    minor changes

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -36,19 +36,22 @@ export default class MyDocument extends Document {
           <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap" rel="stylesheet"/>
         
           <script async src="https://www.googletagmanager.com/gtag/js?id=G-PYQSW4E9S7"
-          />
-        <script
-                  dangerouslySetInnerHTML={{
-                    __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
+          ></script>
+        <Script
+        id ="gtm"
+        stragety="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html:`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','G-PYQSW4E9S7');`
 
-                gtag('config', 'G-PYQSW4E9S7');
-                  }};
-                  `,
-                  }}
-                  />
+        }}
+        >              
+                     
+                  </Script>
         </Head>
         <body>
           <Main />
