c52484acb8ce398798b92578736a67ec8a178808
Diyac004 <diyachaudhari004@gmail.com>
2024-07-24 12:55:59 -0700

    minor changes

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -51,10 +51,6 @@ export default class MyDocument extends Document {
         
         </Head>
         <body>
-       
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T7XVJ9HW"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-
           <Main />
           <NextScript />
         </body>
