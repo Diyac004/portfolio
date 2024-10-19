9b47b2887e05b053cefbb7db8ce715345d9819b4
Diyac004 <diyachaudhari004@gmail.com>
2024-10-19 01:02:54 -0700

    changes to project section and resume

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,89 +1,125 @@
-import React from 'react';
-
-import { BlogCard, CardInfo, ExternalLinks, GridContainer, HeaderThree, Hr, Tag, TagList, TitleContent, UtilityList, Img, VideoContainer,StyledVideo } from './ProjectsStyles';
+import React, { useEffect, useRef } from 'react';
+import { BlogCard, CardInfo, ExternalLinks, GridContainer, HeaderThree, Hr, Tag, TagList, TitleContent, UtilityList, Img, VideoContainer, StyledVideo } from './ProjectsStyles';
 import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+const projectsData = [
+    {
+        title: 'ORBIT',
+        description: 'ORBIT is an AI-powered travel planning tool, designed to efficiently generate personalized itineraries based on user inputs like destination, budget, and travel days. Targeted at frequent travelers and budget-conscious users, it integrates Google authentication, FlightLabs, and Gemini AI to provide real-time travel data, ensuring seamless trip planning. Built using FastAPI, it streamlines the planning process, making it ideal for users seeking both convenience and cost-effectiveness.',
+        image: './images/orbit.png',
+        tags: '[Nextjs, JavaScript, HTML/CSS, MongoDB, Vercel, Google Cloud Console]',
+        code: 'https://github.com/Diyac004/sunhacks-orbit/tree/frontend',
+        demo: 'https://youtu.be/vh-CWneXBWc'
+      },
+  {
+    title: 'All Seeing Boots',
+    description: 'Designed and developed smart shoes for the visually impaired using Raspberry Pi and Python, which enable obstacle detection and assess surface safety. Seamlessly integrated the hardware and software components to enhance the user experience.',
+    image: './images/5.png',
+    tags: '[Raspberry Pi, Python]',
+    teamMembers: 'Ricardo, Kyle, Mackenzie',
+    code: 'https://github.com/memcca14/Master-Project',
+    demo: 'https://youtu.be/_lTmuviq2Y0'
+  },
+  {
+    title: 'Inventory Management',
+    description: 'Developed a comprehensive inventory management web application that allows shop owners to track inventory levels across multiple store locations in real time. It integrates advanced AI algorithms to analyze sales data and provide actionable suggestions on which items to stock more heavily to maximize profits. The intuitive user interface ensures ease of use, while the real-time database management through Firebase guarantees up-to-date inventory tracking and reporting.',
+    image: './images/inventory.png',
+    tags: '[JavaScript, Tailwind CSS, Gemini AI, Firebase]',
+    code: 'https://github.com/Diyac004/inventory-ai',
+    demo: 'https://youtu.be/JMY6sydLozc?si=msDWU2iPIT7alhW5'
+  },
+  {
+    title: 'PromptME',
+    description: 'PromptME is a platform for modern world to discover, create and share their Real-Life experiences and help others in the similar scenarios to make the right decision.',
+    video: './images/promptmevid.mp4',
+    tags: '[Nextjs, JavaScript, HTML/CSS, MongoDB, Vercel, Google Cloud Console]',
+    code: 'https://github.com/Diyac004/prompts',
+    demo: 'https://www.youtube.com/watch?v=UB0M1Uu1zNQ'
+  },
+];
 
-const Projects = () => (
-  
-  <Section nopadding id='projects'>
-    
-    <SectionTitle main>Projects</SectionTitle>
-    <SectionDivider/>
-    
-    <GridContainer >
-   
-        <BlogCard data-aos="zoom-in-up">
-      
-          <Img src="./images/5.png"
-          alt="shoe image" />
-          <TitleContent>
-            <HeaderThree title>All Seeing Boots</HeaderThree>
-            <Hr/>
-            </TitleContent>
-            <CardInfo>Designed and developed smart shoes for the visually impaired using Raspberry Pi and Python, which enable obstacle detection and assess surface safety. Seamlessly integrated the hardware and software components to enhance the user experience.</CardInfo>
-            <div>
-              <TitleContent>Team Members: Ricardo, Kyle, Mackenzie</TitleContent>
-              <TagList>
-             
-                  <Tag>[Raspberry Pi, Python]</Tag>
-                
-              </TagList>
+const Projects = () => {
+  const gridRef = useRef(null);
+  useEffect(()=>{
+    AOS.init()
+  },[]);
+  useEffect(() => {
+    const resizeGridItems = () => {
+      const grid = gridRef.current;
+      if (!grid) return;
+
+      const rowHeight = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-auto-rows'));
+      const rowGap = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-row-gap-2'));
+
+      const items = grid.getElementsByClassName('grid-item');
+      for (let i = 0; i < items.length; i++) {
+        const item = items[i];
+        const contentHeight = item.querySelector('.content').getBoundingClientRect().height;
+        const rowSpan = Math.ceil((contentHeight + rowGap) / (rowHeight + rowGap));
+        item.style.setProperty('--row-span', rowSpan);
+      }
+    };
+
+    resizeGridItems();
+    window.addEventListener('resize', resizeGridItems);
+
+
+    const images = gridRef.current.getElementsByTagName('img');
+    for (let img of images) {
+      img.addEventListener('load', resizeGridItems);
+    }
+
+    return () => {
+      window.removeEventListener('resize', resizeGridItems);
+      for (let img of images) {
+        img.removeEventListener('load', resizeGridItems);
+      }
+    };
+  }, []);
+
+  return (
+    <Section id='Projects'>
+     <SectionTitle data-aos="fade-right">Projects</SectionTitle>
+      <SectionDivider />
+      <GridContainer ref={gridRef}>
+        {projectsData.map((project, index) => (
+          <BlogCard key={index} className="grid-item" data-aos="zoom-in">
+            <div className="content">
+              {project.image ? (
+                <Img src={project.image} alt={project.title} />
+              ) : project.video ? (
+                <VideoContainer>
+                  <StyledVideo>
+                    <source src={project.video} type="video/mp4" />
+                  </StyledVideo>
+                </VideoContainer>
+              ) : null}
+              <TitleContent>
+                <HeaderThree title>{project.title}</HeaderThree>
+                <Hr />
+              </TitleContent>
+              <CardInfo data-aos="zoom-in">{project.description}</CardInfo>
+              {project.teamMembers && (
+                <TitleContent>Team Members: {project.teamMembers}</TitleContent>
+              )}
+              <div>
+                <TagList>
+                  <Tag>{project.tags}</Tag>
+                </TagList>
+              </div>
+              <UtilityList>
+                <ExternalLinks data-aos="flip-down" href={project.code} target="_blank">Code</ExternalLinks>
+                <ExternalLinks data-aos="flip-down" href={project.demo} target="_blank">Video</ExternalLinks>
+              </UtilityList>
             </div>
-          <UtilityList>
-            <ExternalLinks href='https://github.com/memcca14/Master-Project' target='_blank'>Code</ExternalLinks>
-            <ExternalLinks href='https://youtu.be/_lTmuviq2Y0' target='_blank'>Video</ExternalLinks>
-
-          </UtilityList>
           </BlogCard>
-          <BlogCard data-aos="zoom-in-up">
-      
-          <Img src="./images/inventory.png"
-          alt="shoe image"/>
-          <TitleContent>
-            <HeaderThree title>Inventory Management</HeaderThree>
-            <Hr/>
-            </TitleContent>
-            <CardInfo>Developed a comprehensive inventory management web application that allows shop owners to track inventory levels across multiple store locations in real time. It integrates advanced AI algorithms to analyze sales data and provide actionable suggestions on which items to stock more heavily to maximize profits. The intuitive user interface ensures ease of use, while the real-time database management through Firebase guarantees up-to-date inventory tracking and reporting.</CardInfo>
-            <div>
-              <TagList>
-             
-                  <Tag>[JavaScript, Tailwind CSS, Gemini AI, Firebase]</Tag>
-                
-              </TagList>
-            </div>
-          <UtilityList>
-            <ExternalLinks href='https://github.com/Diyac004/inventory-ai' target='_blank'>Code</ExternalLinks>
-            <ExternalLinks href='https://youtu.be/JMY6sydLozc?si=msDWU2iPIT7alhW5' target='_blank'>Video</ExternalLinks>
-
-          </UtilityList>
-          </BlogCard>
-          <BlogCard data-aos="zoom-in-up">
-      
-          <VideoContainer><StyledVideo controls><source src="./images/promptmevid.mp4" type='video/mp4'/> video not found.</StyledVideo></VideoContainer>
-          <TitleContent>
-            <HeaderThree title>PromptME</HeaderThree>
-            <Hr/>
-            </TitleContent>
-            <CardInfo>PromptME is a platform for modern world to discover, create and share their Real-Life experiences and help others in the similar scenarios to make the right decision.</CardInfo>
-            <div>
-              <TagList>
-             
-                  <Tag>[Nextjs, JavaScript, HTML/CSS, MongoDB, Vercel, Google Cloud Console]</Tag>
-                
-              </TagList>
-            </div>
-          <UtilityList>
-            <ExternalLinks href='https://github.com/Diyac004/prompts' target='_blank'>Code</ExternalLinks>
-            <ExternalLinks href='https://www.youtube.com/watch?v=UB0M1Uu1zNQ' target='_blank'>Video</ExternalLinks>
-
-          </UtilityList>
-          </BlogCard>
-          
-     
-     
-    </GridContainer>
-    
-  </Section>
+        ))}
+      </GridContainer>
+    </Section>
   );
+};
+
 
 export default Projects;
+
--- src/components/Projects/ProjectsStyles.js
+++ src/components/Projects/ProjectsStyles.js
@@ -1,51 +1,68 @@
 import styled from 'styled-components';
 
+export const GridContainer = styled.section`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(320px, 1fr));
+  gap: 2rem;
+  padding: 2rem;
+  place-items: start;
+  width: 100%;
+  max-width: auto;
+  margin: 0 auto;
+  grid-auto-rows: auto;
+  @media ${(props) => props.theme.breakpoints.md} {
+    gap: 0.75rem 1rem;
+    padding: 2rem;
+  }
+
+  @media ${(props) => props.theme.breakpoints.sm} {
+    gap: 0.5rem 0.75rem;
+    padding: 1rem;
+  }
+`;
+
 export const BlogCard = styled.div`
-  border-radius: 30px;
+  border-radius: 20px;
   box-shadow: 3px 3px 20px rgba(80, 78, 78, 0.5);
   text-align: center;
-  width: 400px;
-  
+  width: auto;
+  height: 100%;
+  display: flex;
+  margin-bottom: 0.5rem;
+  flex-direction: column;
+  background: rgba(17, 25, 40, 0.25);
+  backdrop-filter: blur(4px);
+  transition: transform 0.2s ease-in-out;
+
+  &:hover {
+    transform: translateY(-5px);
+  }
 `;
+
 export const Img = styled.img`
-border-radius: 30px;
-  width:100%;
-  height:100%;
-  object-fit: contain;
-  overflow: hidden;
-  
-`
-export const VideoContainer = styled.div`
-border-radius: 30px;
+  border-radius: 20px;
   width: 100%;
-  max-width: 800px;
-  margin: 0 auto;
+  height: auto;
+  object-fit: cover;
+`;
+
+export const VideoContainer = styled.div`
+  border-radius: 20px;
+  width: 100%;
+  height: auto;
+  overflow: hidden;
 `;
 
 export const StyledVideo = styled.video.attrs({
-  autoPlay: true
+  autoPlay: true,
+  muted: true,
+  loop: true
 })`
+border-radius: 20px;
   width: 100%;
   height: auto;
-  border-radius: 30px;
+  object-fit: cover;
 `;
-
-
-export const GridContainer = styled.section`
-
-display: flex;
-
-align-items: stretch;
-grid-template-columns: repeat(3, 1fr);
-  gap: 27px;
-grid-template-rows: repeat(auto-fill, minmax(300px, 1fr));
-padding: 4rem;
-place-items: center;
-  @media ${(props) => props.theme.breakpoints.sm} {
-    width: 100%;
-  }
-
-`
 
 export const TitleContent = styled.div`
   text-align: center;
@@ -53,75 +70,79 @@ export const TitleContent = styled.div`
   width: 100%;
 `;
 
-
 export const HeaderThree = styled.h3`
-  font-weight: 400;
+  font-weight: 500;
   letter-spacing: 1px;
   color: #9cc9e3;
-  padding: .1rem 1;
-  font-size: ${(props) => props.title ? '3rem' : '2rem'};
+  padding: 0.5rem;
+  font-size: ${(props) => props.title ? '2.4rem' : '2rem'};
+
+  @media ${(props) => props.theme.breakpoints.sm} {
+    font-size: ${(props) => props.title ? '2rem' : '1.8rem'};
+  }
 `;
 
 export const Hr = styled.hr`
-  width: 100px;
+  width: 50px;
   height: 3px;
   margin: 10px auto;
   border: 0;
   background: #d0bb57;
 `;
 
-export const Intro = styled.div`
-  width: 170px;
-  margin: 0 auto;
-  color: #dce3e7;
-  font-family: 'Droid Serif', serif;
-  font-size: 13px;
-  font-style: italic;
-  line-height: 18px;
-`;
-
-
 export const CardInfo = styled.p`
   width: 100%;
-  padding: 0 20px;
+  padding: 0 2rem;
   color: #e4e6e7;
-  font-style: 5rem;
-  line-height: 20px;
-  text-align: justify;
+  font-size: 1.6rem;
+  line-height: 1.5;
+  text-align: left;
+  flex-grow: 1;
+  margin: 1rem 0;
+
   @media ${(props) => props.theme.breakpoints.sm} {
-    padding:.2rem
-  
-}
+    padding: 1rem;
+    font-size: 1.4rem;
+  }
 `;
 
+export const TagList = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 1rem;
+  padding: 1rem 2rem;
+  margin: 0;
+  list-style: none;
+`;
+
+export const Tag = styled.li`
+  color: #d8bfbf;
+  font-size: 1.4rem;
+  background: rgba(255, 255, 255, 0.1);
+  padding: 0.5rem 1rem;
+  border-radius: 15px;
+`;
 
 export const UtilityList = styled.ul`
   list-style-type: none;
   padding: 0;
   display: flex;
-  justify-content: space-around;
-  margin: 2.5rem;
+  justify-content: center;
+  gap: 1.5rem;
+  margin: 2rem 0;
 `;
 
 export const ExternalLinks = styled.a`
-color:#d4c0c0;
-font-size: 1.6rem;
-padding:1rem 1.5rem;
-background: #6b3030;
-border-radius: 15px;
-transition: 0.5s;
-&:hover{
-  background: #801414;
+  color: #d4c0c0;
+  font-size: 1.4rem;
+  padding: 0.8rem 1.5rem;
+  background: #6b3030;
+  border-radius: 15px;
+  transition: 0.3s;
 
-}
+  &:hover {
+    background: #801414;
+    transform: translateY(-2px);
+  }
 `;
-
-export const TagList = styled.ul`
-display: center;
-justify-content: space-around;
-padding: 2rem;
-`
-export const Tag = styled.li`
-color: #d8bfbf;
-font-size: 1.6rem;
-`
